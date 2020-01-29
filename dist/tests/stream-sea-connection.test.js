b1328c6ed15fd7276d9108b4f0685ff417c4f57f
Sergei Patiakin <sergei.patiakin@gmail.com>
2020-01-29 09:46:23 +0100

    Improve unit test

--- dist/tests/stream-sea-connection.test.js
+++ dist/tests/stream-sea-connection.test.js
@@ -10,12 +10,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const stream_sea_connection_1 = require("../stream-sea-connection");
 const events_1 = require("events");
 const assert = __importStar(require("assert"));
+const stream_sea_subscription_1 = require("../stream-sea-subscription");
 class BasicSocket extends events_1.EventEmitter {
     constructor() {
         super();
-        // public sendMock = jest.fn<any, any>(() => {return;})
-        this.callbacks = [
-            (m) => {
+        this.sendCallbacks = [
+            m => {
                 expect(m.action).toBe('authenticate');
                 this.emit('message', JSON.stringify({
                     id: m.id,
@@ -26,13 +26,34 @@ class BasicSocket extends events_1.EventEmitter {
                     }
                 }));
             },
+            m => {
+                expect(m.action).toBe('subscribe');
+                this.subscriptionKey = m.id;
+                this.emit('message', JSON.stringify({
+                    id: m.id,
+                    action: "subscription",
+                    success: true,
+                    payload: m.id,
+                }));
+            },
         ];
         this.send = (m) => {
-            const fn = this.callbacks.shift();
+            const fn = this.sendCallbacks.shift();
             assert.ok(fn);
             setTimeout(() => fn(JSON.parse(m)));
         };
         setTimeout(() => this.emit('open'));
+    }
+    emitSubscriptionMessage() {
+        assert.ok(this.subscriptionKey);
+        this.emit('message', JSON.stringify({
+            id: this.subscriptionKey,
+            action: 'subscription',
+            streamName: 'testStream',
+            payload: {
+                foo: "bar"
+            },
+        }));
     }
 }
 class BasicSocketFactory {
@@ -45,46 +66,6 @@ class BasicSocketFactory {
         };
     }
 }
-// class UnconnectableConnection extends EventEmitter implements IStreamSeaConnection {
-//   constructor(){
-//     super()
-//     setTimeout(() => {
-//       this.emit('error', 'Could not connect')
-//       this.emit('close')
-//     })
-//   }
-//   addSubscription = () => {return;}
-// }
-// /**
-//  * Factory that creates good connections
-//  */
-// class GoodConnectionFactory implements IStreamSeaConnectionFactory {
-//   public tries = 0
-//   createConnection = () => {
-//     this.tries++
-//     if (this.tries === 1){
-//       return new GoodConnection()
-//     } else {
-//       throw Error('Expected 1 try only')
-//     }
-//   }
-// }
-// /**
-//  * Factory that creates two bad connections followed by a good connection
-//  */
-// class ThirdTimeLuckyConnectionFactory implements IStreamSeaConnectionFactory {
-//   public tries = 0
-//   createConnection = () => {
-//     this.tries++
-//     if (this.tries < 3){
-//       return new UnconnectableConnection()
-//     } else if (this.tries === 3){
-//       return new GoodConnection()
-//     } else {
-//       throw Error('Expected 3 tries only')
-//     }
-//   }
-// }
 describe('StreamSeaConnection', () => {
     it('basic flow', done => {
         const socketFactory = new BasicSocketFactory();
@@ -94,52 +75,17 @@ describe('StreamSeaConnection', () => {
             appSecret: 'test_app_secret',
             socketFactory,
         });
-        // const subscription = new StreamSeaSubscription('testStream')
+        const subscription = new stream_sea_subscription_1.StreamSeaSubscription('testStream');
+        connection.addSubscription(subscription);
         setTimeout(() => {
             expect(socketFactory.sockets.length).toBe(1);
-            expect(socketFactory.sockets[0].callbacks.length).toBe(0);
+            expect(socketFactory.sockets[0].sendCallbacks.length).toBe(0);
             expect(connection.status).toBe(stream_sea_connection_1.StreamSeaConnectionStatus.open);
-            done();
+            subscription.on('message', m => {
+                expect(m.foo).toBe('bar');
+                done();
+            });
+            socketFactory.sockets[0].emitSubscriptionMessage();
         }, 1000);
     });
 });
-// describe('StreamSeaClient', () => {
-//   it('subscription is passed through to the connection', done => {
-//     const connectionFactory = new GoodConnectionFactory()
-//     const client = new StreamSeaClient({
-//       appId: 'mockId',
-//       appSecret: 'mockSecret',
-//       remoteServerHost: 'mockHost',
-//       remoteServerPort: '101',
-//       secure: false,
-//       connectionFactory,
-//     })
-//     const testSubscription = new StreamSeaSubscription('testStream')
-//     ;(client as any).connection.addSubscription = (s: any) => {
-//       // Verify that testSubscription was added to the connection
-//       expect(s).toBe(testSubscription)
-//       done()
-//     }
-//     client.addSubscription(testSubscription)
-//     setTimeout(() => {
-//       expect(connectionFactory.tries).toBe(1)
-//       done()
-//     }, 1000)
-//   })
-//   it('client reconnects on the third attempt if the first two attempts fail', done => {
-//     const connectionFactory = new ThirdTimeLuckyConnectionFactory()
-//     const client = new StreamSeaClient({
-//       appId: 'mockId',
-//       appSecret: 'mockSecret',
-//       remoteServerHost: 'mockHost',
-//       remoteServerPort: '101',
-//       secure: false,
-//       connectionFactory,
-//     })
-//     ;(client as any).RECONNECT_INTERVAL_MS = 1;
-//     setTimeout(() => {
-//       expect(connectionFactory.tries).toBe(3)
-//       done()
-//     }, 1000)
-//   })
-// })
--- src/tests/stream-sea-connection.test.ts
+++ src/tests/stream-sea-connection.test.ts
@@ -1,4 +1,4 @@
-import { IStreamSeaConnection, IStreamSeaConnectionFactory, StreamSeaConnection, StreamSeaConnectionStatus } from "../stream-sea-connection"
+import { StreamSeaConnection, StreamSeaConnectionStatus } from "../stream-sea-connection"
 import { EventEmitter } from "events"
 import { IStreamSeaSocket, IStreamSeaSocketFactory } from "../stream-sea-socket"
 import * as assert from 'assert'
@@ -6,8 +6,9 @@ import { StreamSeaSubscription } from "../stream-sea-subscription"
 
 class BasicSocket extends EventEmitter implements IStreamSeaSocket {
   // public sendMock = jest.fn<any, any>(() => {return;})
-  public callbacks: Array<(m: any) => void> = [
-    (m: any) => {
+  public subscriptionKey?: number
+  public sendCallbacks: Array<(m: any) => void> = [
+    m => {
       expect(m.action).toBe('authenticate')
       this.emit('message', JSON.stringify({
         id: m.id,
@@ -18,15 +19,36 @@ class BasicSocket extends EventEmitter implements IStreamSeaSocket {
         }
       }))
     },
+    m => {
+      expect(m.action).toBe('subscribe')
+      this.subscriptionKey = m.id
+      this.emit('message', JSON.stringify({
+        id: m.id,
+        action: "subscription",
+        success: true,
+        payload: m.id,
+      }))
+    },
   ]
   constructor(){
     super()
     setTimeout(() => this.emit('open'))
   }
   public send = (m: any) => {
-    const fn = this.callbacks.shift()
+    const fn = this.sendCallbacks.shift()
     assert.ok(fn)
     setTimeout(() => fn!(JSON.parse(m)))
+  }
+  public emitSubscriptionMessage(){
+    assert.ok(this.subscriptionKey)
+    this.emit('message', JSON.stringify({
+      id: this.subscriptionKey,
+      action: 'subscription',
+      streamName: 'testStream',
+      payload: {
+        foo: "bar"
+      },
+    }))
   }
 }
 
@@ -39,49 +61,6 @@ class BasicSocketFactory implements IStreamSeaSocketFactory {
   }
 }
 
-// class UnconnectableConnection extends EventEmitter implements IStreamSeaConnection {
-//   constructor(){
-//     super()
-//     setTimeout(() => {
-//       this.emit('error', 'Could not connect')
-//       this.emit('close')
-//     })
-//   }
-//   addSubscription = () => {return;}
-// }
-
-// /**
-//  * Factory that creates good connections
-//  */
-// class GoodConnectionFactory implements IStreamSeaConnectionFactory {
-//   public tries = 0
-//   createConnection = () => {
-//     this.tries++
-//     if (this.tries === 1){
-//       return new GoodConnection()
-//     } else {
-//       throw Error('Expected 1 try only')
-//     }
-//   }
-// }
-
-// /**
-//  * Factory that creates two bad connections followed by a good connection
-//  */
-// class ThirdTimeLuckyConnectionFactory implements IStreamSeaConnectionFactory {
-//   public tries = 0
-//   createConnection = () => {
-//     this.tries++
-//     if (this.tries < 3){
-//       return new UnconnectableConnection()
-//     } else if (this.tries === 3){
-//       return new GoodConnection()
-//     } else {
-//       throw Error('Expected 3 tries only')
-//     }
-//   }
-// }
-
 describe('StreamSeaConnection', () => {
   it('basic flow', done => {
     const socketFactory = new BasicSocketFactory()
@@ -91,52 +70,17 @@ describe('StreamSeaConnection', () => {
       appSecret: 'test_app_secret',
       socketFactory,
     })
-    // const subscription = new StreamSeaSubscription('testStream')
+    const subscription = new StreamSeaSubscription('testStream')
+    connection.addSubscription(subscription)
     setTimeout(() => {
       expect(socketFactory.sockets.length).toBe(1)
-      expect(socketFactory.sockets[0].callbacks.length).toBe(0)
+      expect(socketFactory.sockets[0].sendCallbacks.length).toBe(0)
       expect(connection.status).toBe(StreamSeaConnectionStatus.open)
-      done()
+      subscription.on('message', m => {
+        expect(m.foo).toBe('bar')
+        done()
+      })
+      socketFactory.sockets[0].emitSubscriptionMessage()
     }, 1000)
   })
 })
-// describe('StreamSeaClient', () => {
-//   it('subscription is passed through to the connection', done => {
-//     const connectionFactory = new GoodConnectionFactory()
-//     const client = new StreamSeaClient({
-//       appId: 'mockId',
-//       appSecret: 'mockSecret',
-//       remoteServerHost: 'mockHost',
-//       remoteServerPort: '101',
-//       secure: false,
-//       connectionFactory,
-//     })
-//     const testSubscription = new StreamSeaSubscription('testStream')
-//     ;(client as any).connection.addSubscription = (s: any) => {
-//       // Verify that testSubscription was added to the connection
-//       expect(s).toBe(testSubscription)
-//       done()
-//     }
-//     client.addSubscription(testSubscription)
-//     setTimeout(() => {
-//       expect(connectionFactory.tries).toBe(1)
-//       done()
-//     }, 1000)
-//   })
-//   it('client reconnects on the third attempt if the first two attempts fail', done => {
-//     const connectionFactory = new ThirdTimeLuckyConnectionFactory()
-//     const client = new StreamSeaClient({
-//       appId: 'mockId',
-//       appSecret: 'mockSecret',
-//       remoteServerHost: 'mockHost',
-//       remoteServerPort: '101',
-//       secure: false,
-//       connectionFactory,
-//     })
-//     ;(client as any).RECONNECT_INTERVAL_MS = 1;
-//     setTimeout(() => {
-//       expect(connectionFactory.tries).toBe(3)
-//       done()
-//     }, 1000)
-//   })
-// })
