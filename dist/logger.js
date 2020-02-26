d5fd82621e69286e4f0f3001e714783fa05dd32d
Sergei Patiakin <sergei.patiakin@gmail.com>
2020-02-26 10:46:27 +0100

    Autoformatting

--- dist/logger.js
+++ dist/logger.js
@@ -1,6 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.debug = (...args) => console.debug(...args);
-exports.info = (...args) => console.info(...args);
-exports.warn = (...args) => console.warn(...args);
-exports.error = (...args) => console.error(...args);
+let LOGGER_ENABLED = process.env.NODE_ENV != 'test';
+exports.debug = (...args) => { if (LOGGER_ENABLED)
+    console.debug(...args); };
+exports.info = (...args) => { if (LOGGER_ENABLED)
+    console.info(...args); };
+exports.warn = (...args) => { if (LOGGER_ENABLED)
+    console.warn(...args); };
+exports.error = (...args) => { if (LOGGER_ENABLED)
+    console.error(...args); };
