e69399ad97e950d104842572bed1c297497177f7
Sergei Patiakin <sergei.patiakin@gmail.com>
2020-03-06 13:49:17 +0100

    Add unit test for custom groupId

--- dist/tests/stream-sea-connection.test.js
+++ dist/tests/stream-sea-connection.test.js
@@ -46,6 +46,7 @@ class BasicSocket extends events_1.EventEmitter {
             m => {
                 expect(m.action).toBe('subscribe');
                 this.subscriptionKey = m.id;
+                this.groupId = m.groupId;
                 this.emit('message', {
                     data: JSON.stringify({
                         id: m.id,
@@ -88,7 +89,7 @@ class BasicSocketFactory {
     }
 }
 describe('StreamSeaConnection', () => {
-    it('basic flow', done => {
+    it('positive: default groupId', done => {
         const socketFactory = new BasicSocketFactory();
         const connection = new stream_sea_connection_1.StreamSeaConnection({
             url: 'test_url',
@@ -102,6 +103,8 @@ describe('StreamSeaConnection', () => {
         setTimeout(() => {
             // Verify a socket was created
             expect(socketFactory.sockets.length).toBe(1);
+            // Verify the groupId is undefined
+            expect(socketFactory.sockets[0].groupId).toBe(undefined);
             // Verify that all send callbacks have been called
             expect(socketFactory.sockets[0].sendCallbacks.length).toBe(0);
             // Verify that the connection is open
@@ -114,7 +117,35 @@ describe('StreamSeaConnection', () => {
             socketFactory.sockets[0].emitSubscriptionMessage();
         }, 1000);
     });
-    it('bad credentials', done => {
+    it('positive: custom groupId', done => {
+        const socketFactory = new BasicSocketFactory();
+        const connection = new stream_sea_connection_1.StreamSeaConnection({
+            url: 'test_url',
+            clientId: 'test_client_id',
+            clientSecret: 'test_client_secret',
+            socketFactory,
+            groupId: '00000000-0000-0000-000000001234',
+        });
+        const subscription = new stream_sea_subscription_1.StreamSeaSubscription('testStream');
+        connection.addSubscription(subscription);
+        setTimeout(() => {
+            // Verify a socket was created
+            expect(socketFactory.sockets.length).toBe(1);
+            // Verify the groupId is undefined
+            expect(socketFactory.sockets[0].groupId).toBe('00000000-0000-0000-000000001234');
+            // Verify that all send callbacks have been called
+            expect(socketFactory.sockets[0].sendCallbacks.length).toBe(0);
+            // Verify that the connection is open
+            expect(connection.status).toBe(stream_sea_connection_1.StreamSeaConnectionStatus.open);
+            // Verify that messages on the socket are forwarded to the subscription
+            subscription.on('message', m => {
+                expect(m.foo).toBe('bar');
+                done();
+            });
+            socketFactory.sockets[0].emitSubscriptionMessage();
+        }, 1000);
+    });
+    it('negative: bad credentials', done => {
         const socketFactory = new BasicSocketFactory();
         const connection = new stream_sea_connection_1.StreamSeaConnection({
             url: 'test_url',
