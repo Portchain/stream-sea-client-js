c772f0132e56635ef0266d15a017d9727fab0939
Sergei Patiakin <sergei.patiakin@gmail.com>
2020-03-12 17:21:08 +0100

    Check in assets

--- dist/tests/stream-sea-connection.test.js
+++ dist/tests/stream-sea-connection.test.js
@@ -14,11 +14,14 @@ const stream_sea_subscription_1 = require("../stream-sea-subscription");
 class BasicSocket extends events_1.EventEmitter {
     constructor() {
         super();
+        this.verifyAuthenticationPayload = (payload) => {
+            expect(payload.type).toBe('basic');
+            return payload.clientSecret === 'test_client_secret';
+        };
         this.sendCallbacks = [
             m => {
                 expect(m.action).toBe('authenticate');
-                expect(m.payload.type).toBe('basic');
-                if (m.payload.clientSecret === 'test_client_secret') {
+                if (this.verifyAuthenticationPayload(m.payload)) {
                     this.emit('message', {
                         data: JSON.stringify({
                             id: m.id,
@@ -88,8 +91,27 @@ class BasicSocketFactory {
         };
     }
 }
+class JwtSocket extends BasicSocket {
+    constructor() {
+        super(...arguments);
+        this.verifyAuthenticationPayload = (payload) => {
+            expect(payload.type).toBe('jwt');
+            return payload.jwt === 'test.client.jwt';
+        };
+    }
+}
+class JwtSocketFactory {
+    constructor() {
+        this.sockets = [];
+        this.createSocket = () => {
+            const socket = new JwtSocket();
+            this.sockets.push(socket);
+            return socket;
+        };
+    }
+}
 describe('StreamSeaConnection', () => {
-    it('positive: default groupId', done => {
+    it('positive: Basic auth with default groupId', done => {
         const socketFactory = new BasicSocketFactory();
         const connection = new stream_sea_connection_1.StreamSeaConnection({
             url: 'test_url',
@@ -120,7 +142,7 @@ describe('StreamSeaConnection', () => {
             socketFactory.sockets[0].emitSubscriptionMessage();
         }, 1000);
     });
-    it('positive: custom groupId', done => {
+    it('positive: Basic auth with custom groupId', done => {
         const socketFactory = new BasicSocketFactory();
         const connection = new stream_sea_connection_1.StreamSeaConnection({
             url: 'test_url',
@@ -151,7 +173,7 @@ describe('StreamSeaConnection', () => {
             socketFactory.sockets[0].emitSubscriptionMessage();
         }, 1000);
     });
-    it('negative: bad credentials', done => {
+    it('negative: Basic auth with bad credentials', done => {
         const socketFactory = new BasicSocketFactory();
         const connection = new stream_sea_connection_1.StreamSeaConnection({
             url: 'test_url',
@@ -178,6 +200,37 @@ describe('StreamSeaConnection', () => {
             // Verify that the error handler was called
             expect(errorHandler.mock.calls.length).toBe(1);
             done();
+        }, 1000);
+    });
+    it('positive: JWT auth', done => {
+        const socketFactory = new JwtSocketFactory();
+        const connection = new stream_sea_connection_1.StreamSeaConnection({
+            url: 'test_url',
+            credentialOptions: {
+                type: 'jwt',
+                clientId: 'test_client_id',
+                jwt: 'test.client.jwt',
+            },
+            socketFactory,
+            groupId: undefined,
+        });
+        const subscription = new stream_sea_subscription_1.StreamSeaSubscription('testStream');
+        connection.addSubscription(subscription);
+        setTimeout(() => {
+            // Verify a socket was created
+            expect(socketFactory.sockets.length).toBe(1);
+            // Verify the groupId is undefined
+            expect(socketFactory.sockets[0].groupId).toBe(undefined);
+            // Verify that all send callbacks have been called
+            expect(socketFactory.sockets[0].sendCallbacks.length).toBe(0);
+            // Verify that the connection is open
+            expect(connection.status).toBe(stream_sea_connection_1.StreamSeaConnectionStatus.open);
+            // Verify that messages on the socket are forwarded to the subscription
+            subscription.on('message', m => {
+                expect(m.foo).toBe('bar');
+                done();
+            });
+            socketFactory.sockets[0].emitSubscriptionMessage();
         }, 1000);
     });
 });
